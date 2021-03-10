5a59f11395979ae2d85fda0de01103f961f37857
Vinicius Sousa <csousa.vinicius@gmail.com>
2021-03-10 14:49:55 -0300

    [Flutter] new: unit test step

---
+++ src/br/com/zup/unit_test/UnitTest.groovy
@@ -0,0 +1,36 @@
+
+package br.com.zup.unit_test
+
+class UnitTest{
+    def call (jenkins) {
+        jenkins.podTemplate(
+            containers: [
+                jenkins.containerTemplate(
+                    name: 'flutter', 
+                    image: 'cirrusci/flutter:2.0.1', 
+                    ttyEnabled: true, 
+                    command: 'cat'
+                )
+            ],
+            yamlMergeStrategy: jenkins.merge(),
+            workspaceVolume: jenkins.persistentVolumeClaimWorkspaceVolume(
+                claimName: "pvc-${jenkins.env.JENKINS_AGENT_NAME}",
+                readOnly: false
+            )
+        )
+
+        {
+            jenkins.node(jenkins.POD_LABEL){
+                jenkins.container('flutter'){
+                    try {
+                        jenkins.sh label: "Unit Tests", 
+                                   script: "flutter test --coverage test/*"
+                    } catch (Exception e) {
+                        jenkins.unstable("AN error occured during build step. Please, verify the logs.")
+                    }
+                    
+                }
+            }
+        }
+    }
+}
